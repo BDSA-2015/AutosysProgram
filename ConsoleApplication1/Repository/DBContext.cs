c2fa11bb8a8cb93d18397236b5cf45d5eac513fd
William <william.marstrand@gmail.com>
2015-11-11 23:54:11 +0100

    IStorage Interface added, DbContext added, Product class initialized

---
+++ ConsoleApplication1/Repository/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.ApplicationLogic.PaperManagement;
+
+namespace ConsoleApplication1.Repository
+{
+    class DBContext : DbContext
+    {
+        public DbSet<Paper> Papers { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Repository/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Repository
+{
+    interface IStorage<T> where T : class
+    {
+        void Create(T item);
+        T Read(int id);
+        IEnumerable<T> Read();
+        void Update(T item);
+        void Delete(T item);
+    }
+}
