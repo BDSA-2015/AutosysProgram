71647b830935f1efa7f3f1b3470c3b693a4e306e
JakeMullit <jacob.moiniche@hotmail.com>
2015-11-12 15:18:01 +0100

    Added a code sceleton for Phase and added empty class Criteria

---
+++ ApplicationLogics/StudyManagement/Phase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLogics.StudyManagement
+{
+    public class Phase
+    {
+        public List<Criteria> Criterias { get; protected set; }
+
+
+        public bool HasCriteria()
+        {
+         throw new NotImplementedException();   
+        }
+
+        public void AddCriteria()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCriteria()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
