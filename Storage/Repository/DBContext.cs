004771ab70c79b151eca38fa83ea75372861e9a5
Dennis <dttn@itu.dk>
2015-11-12 15:11:20 +0100

    Fixed wrong DBContext reference and recreated applicationlogictests

--- Storage/Repository/DBContext.cs
+++ Storage/Repository/DBContext.cs
@@ -4,12 +4,11 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApplication1.ApplicationLogic.PaperManagement;
 
-namespace ConsoleApplication1.Repository
+namespace Storage.Repository
 {
     class DBContext : DbContext
     {
-        public DbSet<Paper> Papers { get; set; }
     }
+    
 }
