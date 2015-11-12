096c9cfb5859dbfcc3ea01237227252cdfb79c51
JakeMullit <jacob.moiniche@hotmail.com>
2015-11-12 15:57:28 +0100

    Phase,  is now ready to be implemented

--- ApplicationLogics/StudyManagement/Criteria.cs
+++ ApplicationLogics/StudyManagement/Criteria.cs
@@ -8,9 +8,27 @@ namespace ApplicationLogics.StudyManagement
 {
     public class Criteria
     {
+        public string CriteriaName { get; protected set;}
+        public string Description { get; protected set;}
+
         public Criteria()
         {
-            
+            throw new NotImplementedException();
         }
+
+
+        public void ChangeCriteriaName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
+
+    
 }
--- ApplicationLogics/StudyManagement/Phase.cs
+++ ApplicationLogics/StudyManagement/Phase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,71 @@ namespace ApplicationLogics.StudyManagement
     public class Phase
     {
         public List<Criteria> Criterias { get; protected set; }
+        public Dictionary<User,Task> AssignedTask { get; protected set; }
+
+        public List<Task> UnAssignedTasks { get; protected set; }
+
+        public bool PhaseFinished { get; protected set; }
+
+        public List<Phase> DependentPhases { get; protected set; }
 
 
-        public bool HasCriteria()
+        public bool HasCriteria(Criteria criteria)
         {
          throw new NotImplementedException();   
         }
 
-        public void AddCriteria()
+        public void AddCriteria(Criteria criteria)
         {
             throw new NotImplementedException();
         }
 
-        public void RemoveCriteria()
+        public void RemoveCriteria(string criteriaName)
         {
             throw new NotImplementedException();
+        }
+
+
+        public void AddUserToTask(User user)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveUserFromTask(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void AddTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTask(string taskName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public IEnumerable<Task> GetUnfinishedTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDependency(Phase phase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveDependency(Phase pahse)
+        {
+            
         }
     }
 }
