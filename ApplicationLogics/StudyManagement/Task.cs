30e8a651219eb7a89b1857e7549f0616d9f1a34c
TVAO <thorolesen14@gmail.com>
2015-11-12 15:49:44 +0100

    Changed Storage structure and added IEntity interface

---
+++ ApplicationLogics/StudyManagement/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationLogics.Repository;
+
+namespace ApplicationLogics.StudyManagement
+{
+    public class Task : IEntity
+    {
+        public int Id { get; set; }
+        public enum Types { FillData, ResolveConflict }
+        
+    }
+}
