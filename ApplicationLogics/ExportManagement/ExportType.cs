73a64e3ba0d05b3d3b11af1dfda3181cffbb2f23
William <william.marstrand@gmail.com>
2015-11-15 18:09:15 +0100

    ExportType Enum and ExportHandler skeleton done

---
+++ ApplicationLogics/ExportManagement/ExportType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLogics
+{
+    public class ExportType
+    {
+        public enum ExportType { CSV, PDF};
+    }
+}
