7e7970b3bb44379f3ef1623a51589a1689f77a40
JakeMullit <jacob.moiniche@hotmail.com>
2015-11-15 14:47:40 +0100

    Start of implementation

--- ApplicationLogics/PaperManagement/Paper.cs
+++ ApplicationLogics/PaperManagement/Paper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using ApplicationLogics.Repository;
@@ -10,6 +12,29 @@ namespace ApplicationLogics.PaperManagement
     public class Paper : IEntity
     {
         public int Id { get; set; }
+
+        public Dictionary<Tag, List<String>> PaperInformation { get; protected set; }
+
+
+        public Paper()
+        {
+            throw  new NotImplementedException();
+        }
+
+        public Paper(Dictionary<Tag, List<String>> PaperInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddInformation(Tag tag, string information)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveInformation()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
