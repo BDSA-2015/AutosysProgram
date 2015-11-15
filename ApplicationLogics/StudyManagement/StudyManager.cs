91a992fd8c9adc6c227a4dd47ecf5af49589bd1d
Jacob Mullit Møiniche <jacob.moiniche@hotmail.com>
2015-11-15 19:41:17 +0100

    Initial commit

---
+++ ApplicationLogics/StudyManagement/StudyManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLogics.StudyManagement
+{
+    class StudyManager
+    {
+        public Phase currentPhase { get; protected set; }
+
+        //The key is the user's ID and the values are his roles in the current phase
+        public  Dictionary<int, Role> RolesInPhase { get; protected set; }
+
+        //A map of tasks in the current phase, each task can have multiple 
+        public Dictionary<Task, List<int>> TaskInPhase { get; protected set; }
+
+
+        public void AddRole(int UserId, Role role = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRole(int userId, Role role = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAllRoles(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTask(Task task, List<int> participant = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveMemberFromTask(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
