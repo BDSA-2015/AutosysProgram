d129050f9ac5e262707d410b52f6834f08fab44b
Jacob Mullit Møiniche <jacob.moiniche@hotmail.com>
2015-11-16 20:36:51 +0100

    Task reimagined in the creators image

--- ApplicationLogics/StudyManagement/Task.cs
+++ ApplicationLogics/StudyManagement/Task.cs
@@ -14,9 +14,52 @@ namespace ApplicationLogics.StudyManagement
         /// <summary>
         /// What state is the task in. Has it not been initialized yet, is it in progress or has it been completed
         /// </summary>
-        public enum Types { NotStarted, Started, Done }
+        public enum Progress { NotStarted, Started, Done }
 
-        // Data fields, still do not know data type?
-        public object PropertyValue { get; set; }
+        public enum TaskType {FillOutDataFields, HandleConflictingDatafields }
+
+        /// <summary>
+        /// A brief description of the task
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// What kind of task is this
+        /// </summary>
+       
+
+        private List<DataField> NonModifiableDatafields { get; set; }
+        public List<DataField> ModifiableDatafields {  get;  set; } //TODO CONSIDER IF WE WANT TO RETURN THE WHOLE LIST.
+
+        private List<DataField> _nonModifiableDatafields;
+
+        public IEnumerable<DataField> getNonModifiableDatafields()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class DataField
+        {
+            string Name { get; set; }
+            public bool IsModifiable { get; private set; }
+
+            public Object Value { get; private set; }
+
+            public DataField(string Name, Object value, bool IsModifiable)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void setValue(Object value)
+            {
+                throw new NotImplementedException();
+                if (IsModifiable)
+                {
+
+
+                }
+
+            }
+
+        }
     }
 }
