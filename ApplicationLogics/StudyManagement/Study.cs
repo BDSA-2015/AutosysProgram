aa1030e09dffb57bb9475bddcf3e7a94be62ca49
Jacob Mullit Møiniche <jacob.moiniche@hotmail.com>
2015-11-16 19:12:15 +0100

    Added comments to StudyManagement files

--- ApplicationLogics/StudyManagement/Study.cs
+++ ApplicationLogics/StudyManagement/Study.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationLogics.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,24 @@ using System.Threading.Tasks;
 
 namespace ApplicationLogics.StudyManagement
 {
-    class Study
+    class Study: IEntity
     {
+        public int Id { get; set; }
+        /// <summary>
+        /// The Study's name. Try to keep the name unique
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// What kind of study  is this
+        /// </summary>
         public string Classification { get; set; }
+        /// <summary>
+        /// A quick summary of the study
+        /// </summary>
         public string description { get; set; }
+        /// <summary>
+        /// The phases the project has been through and the current phase
+        /// </summary>
         public List<Phase> phases { get;  set; }
 
         
--- ApplicationLogics/StudyManagement/Task.cs
+++ ApplicationLogics/StudyManagement/Task.cs
@@ -11,6 +11,9 @@ namespace ApplicationLogics.StudyManagement
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// What state is the task in. Has it not been initialized yet, is it in progress or has it been completed
+        /// </summary>
         public enum Types { NotStarted, Started, Done }
 
         // Data fields, still do not know data type?
