bcc71024806d607412d5dd12d2a940df98f650dd
William <william.marstrand@gmail.com>
2015-11-24 10:39:29 +0100

    CsvFile is now included in the project during the compile

---
+++ ApplicationLogics/ExportManagement/CsvFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationLogics.StudyManagement;
+
+namespace ApplicationLogics.ExportManagement
+{
+    public class CsvFile : IExportFile
+    {
+        public int Id { get; set; }
+        public ExportType Type { get; set; }
+        public string InclusionCriteria { get; set; }
+        public string ExclusionCriteria { get; set; }
+        public int Origin { get; set; }
+        public string Description { get; set; }
+    }
+}
--- ApplicationLogics/ExportManagement/PDFConverter.cs
+++ ApplicationLogics/ExportManagement/PDFConverter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ApplicationLogics.StudyManagement;
+using Newtonsoft.Json;
 
 namespace ApplicationLogics.ExportManagement
 {
@@ -14,14 +15,19 @@ namespace ApplicationLogics.ExportManagement
     //Class is under construction
     public class PDFConverter : IConverter
     {
-        public IExportFile Convert(Protocol protocol)
+        /// <summary>
+        /// Serializes the given Protocol and returns it as a JSON string
+        /// </summary>
+        /// <param name="protocol">The Protocol to be exported</param>
+        /// <returns>A Protocol serialized to a JSON string</returns>
+        public string Convert(Protocol protocol)
         {
             var exportFile = new PdfFile();
             exportFile.Type = ExportType.PDF;
             exportFile.Description = protocol.Description;
             exportFile.Origin = protocol.Id;
             exportFile.Bytes = System.Convert.FromBase64String(ConvertInclusionData(protocol));
-            return exportFile;
+            return JsonConvert.SerializeObject(exportFile);
         }
 
         private string ConvertInclusionData(Protocol protocol)
