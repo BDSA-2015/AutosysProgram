bfa5a4c566513efa8da221ec84f925b9e3606ff0
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2015-11-28 18:26:24 +0100

    Added front page with buttons Added Study creation page (Blank page) Added Viewmodel for MainPage with version

---
+++ StudyConfigurationUI/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using StudyConfigurationUI.Annotations;
+
+namespace StudyConfigurationUI.ViewModel
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        private double _version = 0.1;
+        public string VersionText => "Autosys Studyconfiguation version "+ _version;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
