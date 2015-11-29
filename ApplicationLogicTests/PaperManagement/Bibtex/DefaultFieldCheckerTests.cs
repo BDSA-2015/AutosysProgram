3d459d89342365679fec5f312bb5acf44e502691
William <william.marstrand@gmail.com>
2015-11-29 23:17:59 +0100

    Defaultchecker implementation

---
+++ ApplicationLogicTests/PaperManagement/Bibtex/DefaultFieldCheckerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using ApplicationLogics.PaperManagement.Bibtex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApplicationLogicTests.PaperManagement.Bibtex
+{
+    [TestClass()]
+    public class DefaultFieldCheckerTests
+    {
+        [TestMethod()]
+        public void ValidateValidInputTest()
+        {
+            //Arrange
+            DefaultFieldChecker checker = new DefaultFieldChecker();
+            var bibtexInput1 = "T";
+            var bibtexInput2 = "Testing";
+            var bibtexInput3 = "Testing Space";
+            var bibtexInput4 = "Testing Numbers 123";
+            var bibtexInput5 = "Testing Symbols !@#$%^&*()_+?|>\\/,.;':\"][}{";
+            var bibtexInput6 = "Something\n";
+
+            //Assert
+            Assert.IsTrue(checker.Validate(bibtexInput1));
+            Assert.IsTrue(checker.Validate(bibtexInput2));
+            Assert.IsTrue(checker.Validate(bibtexInput3));
+            Assert.IsTrue(checker.Validate(bibtexInput4));
+            Assert.IsTrue(checker.Validate(bibtexInput5));
+            Assert.IsTrue(checker.Validate(bibtexInput6));
+        }
+
+        [TestMethod()]
+        public void ValidateNewLineInputTest()
+        {
+            //Arrange
+            DefaultFieldChecker checker = new DefaultFieldChecker();
+            var bibtexInput1 = "\nTest";
+            var bibtexInput2 = "Something\n more";
+
+            //Assert
+            Assert.IsFalse(checker.Validate(bibtexInput1));
+            Assert.IsFalse(checker.Validate(bibtexInput2));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidateEmptyInputTest()
+        {
+            //Arrange
+            DefaultFieldChecker checker = new DefaultFieldChecker();
+            var bibtexInput1 = "";
+            
+            //Assert
+            Assert.IsFalse(checker.Validate(bibtexInput1));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidateNullInputTest()
+        {
+            //Arrange
+            DefaultFieldChecker checker = new DefaultFieldChecker();
+
+            //Act
+            checker.Validate(null);
+        }
+    }
+}
--- ApplicationLogics/PaperManagement/Bibtex/DefaultFieldChecker.cs
+++ ApplicationLogics/PaperManagement/Bibtex/DefaultFieldChecker.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using ApplicationLogics.PaperManagement.Interfaces;
 
 namespace ApplicationLogics.PaperManagement.Bibtex
 {
     public class DefaultFieldChecker : IFieldChecker
     {
+        //Matches all strings without a new line at the beginning or in between characters.
+        readonly Regex _r = new Regex("^.*$");
+
         public bool Validate(string tag)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(tag))
+            {
+                throw new ArgumentNullException("The given bibtex field cannot be null or empty");
+            }
+            return _r.IsMatch(tag);
         }
     }
 }
