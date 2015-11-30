74e1dc73c49f8dd4081cd628ff7ba4e39acf8561
William <william.marstrand@gmail.com>
2015-11-30 12:43:47 +0100

    PaperValidator implementation

---
+++ ApplicationLogicTests/PaperManagement/Bibtex/DefaultPaperCheckerTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationLogics.PaperManagement.Bibtex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApplicationLogicTests.PaperManagement.Bibtex
+{
+    [TestClass()]
+    public class DefaultPaperCheckerTests
+    {
+        [TestMethod()]
+        public void ValidateValidInputTest()
+        {
+            //Arrange
+            DefaultPaperChecker checker = new DefaultPaperChecker();
+            var fields = new Dictionary<EnumField, string>
+            {
+                {EnumField.Year, "2015"},
+                {EnumField.Author, "William McSomething"}
+            };
+            var paper = new Paper(EnumEntry.Phdthesis, fields);
+
+            //Assert
+            Assert.IsTrue(checker.Validate(paper));
+        }
+
+        [TestMethod()]
+        public void ValidateInvalidFieldTest()
+        {
+            //Arrange
+            DefaultPaperChecker checker = new DefaultPaperChecker();
+            var fields = new Dictionary<EnumField, string> {{EnumField.Author, "\nInvalid"}};
+            var paper = new Paper(EnumEntry.Phdthesis, fields);
+
+            //Assert
+            Assert.IsFalse(checker.Validate(paper));
+        }
+
+        //[TestMethod()]
+        //public void ValidateInvalidEntryTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+        //    var fields = new Dictionary<EnumField, string>();
+        //    fields.Add(EnumField.Year, "2015");
+        //    fields.Add(EnumField.Author, "William McSomething");
+        //    var paper = new Paper("%maliciousPaper", fields);
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(paper));
+        //}
+
+        //[TestMethod()]
+        //public void ValidateEmptyEntryTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+        //    var fields = new Dictionary<EnumField, string>();
+        //    fields.Add("year", "2015");
+        //    fields.Add("Author", "William McSomething");
+        //    var paper = new Paper("", fields);
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(paper));
+        //}
+
+        //[TestMethod()]
+        //public void ValidateEmptyFieldTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+        //    var fields = new Dictionary<EnumField, string>();
+        //    fields.Add("", "2015");
+        //    var paper = new Paper("book", fields);
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(paper));
+        //}
+
+        //[TestMethod()]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void ValidateNullEntryTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+        //    var fields = new Dictionary<EnumField, string>();
+        //    fields.Add("year", "2015");
+        //    var paper = new Paper(null, fields);
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(paper));
+        //}
+
+        //[TestMethod()]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void ValidateNullPaperTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(null));
+        //}
+
+        //[TestMethod()]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void ValidateEmptyFieldsTest()
+        //{
+        //    //Arrange
+        //    DefaultPaperChecker checker = new DefaultPaperChecker();
+        //    var fields = new Dictionary<EnumField, string>();
+        //    var paper = new Paper("article", fields);
+
+        //    //Assert
+        //    Assert.IsFalse(checker.Validate(paper));
+        //}
+    }
+}
---
+++ ApplicationLogicTests/PaperManagement/Bibtex/PaperValidatorTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationLogics.PaperManagement.Bibtex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApplicationLogicTests.PaperManagement.Bibtex
+{
+    [TestClass()]
+    public class PaperValidatorTests
+    {
+
+        [TestMethod()]
+        public void IsPaperValidRequiredFieldsTest()
+        {
+            //Arrange
+            var validator = new PaperValidator();
+            var fields = new Dictionary<EnumField, string>()
+            {
+                {EnumField.Author, "Professor Clever"},
+                {EnumField.Year, "2015"},
+                {EnumField.Title, "How to see sharp" }
+            };
+            var paper = new Paper(EnumEntry.Article, fields);
+
+            //Assert
+            Assert.IsTrue(validator.IsPaperValid(paper));
+        }
+
+        [TestMethod()]
+        public void IsPaperValidMissingAuthorTest()
+        {
+            //Arrange
+            var validator = new PaperValidator();
+            var fields = new Dictionary<EnumField, string>()
+            {
+                {EnumField.Year, "2015"},
+                {EnumField.Booktitle, "How to see sharp" }
+            };
+            var paper = new Paper(EnumEntry.Article, fields);
+
+            //Assert
+            Assert.IsFalse(validator.IsPaperValid(paper));
+        }
+
+        [TestMethod()]
+        public void IsPaperValidMissingTitleTest()
+        {
+            //Arrange
+            var validator = new PaperValidator();
+            var fields = new Dictionary<EnumField, string>()
+            {
+                {EnumField.Author, "Professor Clever"},
+                {EnumField.Year, "2015"},
+            };
+            var paper = new Paper(EnumEntry.Article, fields);
+
+            //Assert
+            Assert.IsFalse(validator.IsPaperValid(paper));
+        }
+
+        [TestMethod()]
+        public void IsPaperValidMissingYearTest()
+        {
+            //Arrange
+            var validator = new PaperValidator();
+            var fields = new Dictionary<EnumField, string>()
+            {
+                {EnumField.Author, "Professor Clever"},
+                {EnumField.Booktitle, "How to see sharp" }
+            };
+            var paper = new Paper(EnumEntry.Article, fields);
+
+            //Assert
+            Assert.IsFalse(validator.IsPaperValid(paper));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IsPaperValidNullTest()
+        {
+            //Arrange
+            var validator = new PaperValidator();
+
+            //Assert
+            Assert.IsFalse(validator.IsPaperValid(null));
+        }
+    }
+}
