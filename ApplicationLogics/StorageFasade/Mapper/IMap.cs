e63e97b8a2e425432409c697beb987cb1580c204
Dennis <dttn@itu.dk>
2015-12-01 11:33:43 +0100

    Added IMap interface implemented in seperated mapper classes for each facade. This will translate DTOs to objects in application logic and application logic objects to model objects in storage.

---
+++ ApplicationLogics/StorageFasade/Mapper/IMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ApplicationLogics.StorageFasade.Mapper
+{
+    /// <summary>
+    /// This interface outlines the method used to mutually convert DTOs, 
+    /// Objects in application logic and model objects in storage.
+    /// This will translate DTOs to objects in application logic 
+    /// and application logic objects to model objects in storage.
+    /// For mapping use following package through NuGet: Install-Package AutoMapper
+    /// 
+    /// To map an object do the following:
+    /// https://github.com/AutoMapper/AutoMapper/wiki/Getting-started
+    /// </summary>
+    interface IMap<T, K> 
+        where T : class 
+        where K : class
+    {
+        T Map(K item);
+        K Map(T item);
+    }
+}
