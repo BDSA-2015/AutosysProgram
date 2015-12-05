9515cb31db752d83ac278acc3981bc127b538a11
TVAO <thorolesen14@gmail.com>
2015-12-05 15:51:35 +0100

    Added ProtocolRepository unit test.

---
+++ StorageTests/ProtocolRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StorageTests
+{
+
+    [TestClass]
+    public class ProtocolRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+
+}
