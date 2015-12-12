98138300350b3efdab0fd14042864dfd91859eac
Jacob Mullit Møiniche <jacob.moiniche@hotmail.com>
2015-12-12 22:36:37 +0100

    APi- save changes to be able to move to laptop

--- WebApi/Mapping/Profiles/StudyEntitiesMapping.cs
+++ WebApi/Mapping/Profiles/StudyEntitiesMapping.cs
@@ -3,10 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DatabaseTask = ApplicationLogics.StudyManagement.TaskRequest;
+using TaskRequest = WebApi.Models.TaskRequest;
+
 
 namespace WebApi.Mapping
 {
     public class StudyEntitiesMapping : Profile
     {
+        public void CreateRoleMapping()
+        {
+            Mapper.CreateMap<DatabaseTask, TaskRequest, TaskRequest>();
+        }
     }
 }
