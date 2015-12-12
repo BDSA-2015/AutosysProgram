2b624e27445a102b88a8403cd879df0d184b99da
William <william.marstrand@gmail.com>
2015-12-12 19:01:45 +0100

    Added: Documentation for BibtexTag class

--- ApplicationLogics/PaperManagement/BibtexTag.cs
+++ ApplicationLogics/PaperManagement/BibtexTag.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace ApplicationLogics.PaperManagement
 {
+    /// <summary>
+    /// This class represents a bibtex tag which can both be an entry e.g. article or book and also a field type e.g. author or year.
+    /// A bibtex tag is created from a bibtex file and stored as a StoredBibtexTag in the database
+    /// </summary>
     public class BibtexTag
     {
+        /// <summary>
+        /// Type of the bibtex tag e.g. article, book, author, or year
+        /// </summary>
         public string Type { get; set; }
     }
 }
