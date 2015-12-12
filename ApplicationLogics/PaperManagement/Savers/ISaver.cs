312f674f83ed4cb421214a863212efceea5e9f7a
William <william.marstrand@gmail.com>
2015-12-12 21:27:31 +0100

    Implementation: Implemented ISaver interface

---
+++ ApplicationLogics/PaperManagement/Savers/ISaver.cs
@@ -0,0 +1,15 @@
+﻿namespace ApplicationLogics.PaperManagement.Savers
+{
+    /// <summary>
+    /// Interface for saver classes which are used to save new tags in the database 
+    /// when a file with unknown tags is imported into the system
+    /// </summary>
+    public interface ISaver
+    {
+        /// <summary>
+        /// Saves the tags from a file in the database if the tags does not exist in the database
+        /// </summary>
+        /// <param name="file"></param>
+        void Save(string file);
+    }
+}
