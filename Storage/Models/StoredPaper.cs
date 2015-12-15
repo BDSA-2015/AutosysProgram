aef35e5436e5cec1fd5afa617d2dc0b44ab235d1
TVAO <thorolesen14@gmail.com>
2015-12-15 12:29:45 +0100

    Updated StoredPaper and documentation.

--- Storage/Models/StoredPaper.cs
+++ Storage/Models/StoredPaper.cs
@@ -12,14 +12,31 @@ namespace Storage.Models
     [Table("Paper")]
     public class StoredPaper : IEntity
     {
+
+        /// <summary>
+        ///     The Entry type of the bibliographic item (e.g. Article, Book, Phdthesis...)
+        /// </summary>
         [Required]
         public string Type { get; set; }
 
+        /// <summary>
+        ///     A collection of bibtex field types (e.g. Author, Year...)
+        ///     The collection holds information associated with the bibtex field values in the FieldValues collection
+        /// </summary>
         [Required]
-        public virtual ICollection<string> FieldTypes { get; set; }
+        public virtual ICollection<string> FieldTypes { get; set; } // TODO map to IReadOnlyCollection in logic 
 
+        /// <summary>
+        ///     A collection of bibtex field values (e.g. Henrik Madsen, 2015)
+        ///     The collection holds information associated with the bibtex field types in the FieldTypes collection
+        /// </summary>
         [Required]
-        public virtual ICollection<string> FieldValues { get; set; }
+        public virtual ICollection<string> FieldValues { get; set; } // TODO map to IReadOnlyCollection in logic 
+
+        /// <summary>
+        ///     A collection of custom made data fields holding information related to a specific Paper
+        /// </summary>
+        public virtual ICollection<StoredDataField> RequesteDataFields { get; set; }
 
         /// <summary>
         ///     A reference to the resource associated with this Paper (e.g. A PDF, or CSV file...)
